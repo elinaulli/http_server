12ff6555a47015bd0e9e9f4648ee1ca00a9864bb
Elina Islamova <162158747+elinaulli@users.noreply.github.com>
2025-12-20 15:34:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ class Ticket {
   constructor(id, name, status, created) {
     this.id = id;
     this.name = name;
-    this.status = status;
+    this.status = Boolen(status);
     this.created = created;
   }
 }
@@ -62,7 +62,7 @@ class TicketFull {
       this.#nextId++,
       name,
       description,
-      status,
+      finalStatus,
       new Date()
     );
     this.#tickets.push(ticket);
